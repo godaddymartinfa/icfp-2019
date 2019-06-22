ad2acda37111291e0d30162416029184ff2f1dee
Martin Fahy <mfahy@godaddy.com>
2019-06-22 15:06:13 -0700

    Current Backtracking Strategy

--- src/main/kotlin/icfp2019/BackTrackingStrategy.kt
+++ src/main/kotlin/icfp2019/BackTrackingStrategy.kt
@@ -1,93 +1,34 @@
 package icfp2019
 
+import org.jgrapht.graph.DefaultEdge
+import org.jgrapht.graph.SimpleGraph
+import org.jgrapht.traverse.DepthFirstIterator
+import org.jgrapht.traverse.GraphIterator
 import java.util.*
 
 // Move to an open space and push moves onto a stack, if no moves available then backtrack using the stack
 class BackTrackingStrategy : Strategy {
-    override fun getActions(gameState: GameState): Map<RobotId, List<Action>> {
-        val stack = Stack<Action>()
-        val moveList = mutableListOf<Action>()
-        val tEngine = TransitionEngine()
-
-        var currentState = gameState
-        var availableMoves = availableMoves(gameState)
-        do {
-            if (availableMoves.isEmpty()) {
-                // Backtrack if we can
-                if (stack.isEmpty()) {
-                    break
-                } else {
-                    val backTrackAction = stack.pop().invert() ?: throw IllegalStateException("Non-invertable action!")
-
-                    currentState = tEngine.apply(
-                        currentState, hashMapOf(
-                            Pair<RobotId, Action>(RobotId(0), backTrackAction)
-                        )
-                    )
-                    stack.push(backTrackAction)
-                    moveList.add(backTrackAction)
-                    availableMoves = availableMoves(currentState)
+    // Returns the problem output string
+    override fun getActions(undirectedGraph: SimpleGraph<Node, DefaultEdge>, gameState: GameState): String {
+        val it: GraphIterator<Node, DefaultEdge>  = DepthFirstIterator<Node, DefaultEdge>(undirectedGraph)
+        val visitedMap = mutableMapOf<Node, Boolean>()
+        while(it.hasNext()) {
+            val currentNode: Node = it.next()
+            if(!visitedMap.containsKey(currentNode)) {
+                // Consume the node if we haven't seen the node before
+                when(nodeIsConsumable(currentNode)) {
+                    true -> gameStateMovement()
                 }
-            } else {
-                // Else take the first available move
-                val action = availableMoves.get(0)
-                currentState = tEngine.apply(
-                    currentState, hashMapOf(
-                        Pair<RobotId, Action>(RobotId(0), action)
-                    )
-                )
-                stack.push(action)
-                moveList.add(action)
-                availableMoves = availableMoves(currentState)
             }
-        } while (stack.isNotEmpty() || availableMoves.isNotEmpty())
-
-        val plans = hashMapOf<RobotId, List<Action>>(
-            Pair<RobotId, List<Action>>(RobotId(0), moveList)
-        )
-
-        return plans
+        }
+        return movement;
     }
 
-    fun availableMoves(gameState: GameState): List<Action> {
-        val moves = mutableListOf<Action>()
+    private fun nodeIsConsumable(currentNode: Node): Boolean{
+        return !currentNode.isWrapped
+    }
 
-        val currentPosition = gameState.robotStateList[0].currentPosition
+    private fun gameStateMovement() {
 
-        fun canMoveTo(node: Node): Boolean {
-            return node.isWrapped.not() && node.isObstacle.not()
-        }
-
-        if (currentPosition.y + 1 < gameState.gameBoard.height) {
-            val up = gameState.gameBoard.get(currentPosition.up())
-            if (canMoveTo(up)) {
-                moves.add(Action.MoveUp)
-            }
-        }
-
-        if (currentPosition.y - 1 > -1) {
-            val down = gameState.gameBoard.get(currentPosition.down())
-            if (canMoveTo(down)) {
-                moves.add(Action.MoveDown)
-            }
-        }
-
-        if (currentPosition.x - 1 > -1) {
-            val left = gameState.gameBoard.get(currentPosition.left())
-            if (canMoveTo(left)) {
-                moves.add(Action.MoveLeft)
-            }
-        }
-
-        if (currentPosition.x + 1 < gameState.gameBoard.width) {
-            val right = gameState.gameBoard.get(currentPosition.right())
-            if (canMoveTo(right)) {
-                moves.add(Action.MoveRight)
-            }
-        }
-
-        // do we care about the last move so we continue in that direction? would need it first in the list
-
-        return moves
     }
 }
--- src/main/kotlin/icfp2019/Strategy.kt
+++ src/main/kotlin/icfp2019/Strategy.kt
@@ -1,6 +1,9 @@
 package icfp2019
 
+import org.jgrapht.graph.DefaultEdge
+import org.jgrapht.graph.SimpleGraph
+
 interface Strategy {
     // Given a state, return a list of actions, indexed by Robot id.
-    fun getActions(gameState: GameState): Map<RobotId, List<Action>>
+    fun getActions(undirectedGraph: SimpleGraph<Node, DefaultEdge>, gameState: GameState): String
 }
